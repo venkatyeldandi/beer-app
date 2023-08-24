a65fd5e354361d045e2ea01e93dd3f8d9783a15b
Venkat Yeldandi <venkatyeldandi@example.com>
2023-08-24 22:49:28 +0530

    Initial commit

---
+++ src/AuthContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState, useContext } from 'react';
+const AuthContext = createContext();
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isClicked,setIsClicked] = useState(false);
+
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const value = {
+    isLoggedIn,
+    login,
+    logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
